083fb1b6f1eeb89fbef6bad503040e6cf317265e
jwc20 <choij2015@gmail.com>
2022-08-15 13:38:32 -0700

    ics stuff

--- src/utils.js
+++ src/utils.js
@@ -15,3 +15,38 @@ export const formatTime = (mSec) => {
     (ms > 99 ? ms : ms > 9 ? "0" + ms : "00" + ms)
   );
 };
+
+export const makeEvent = () => {
+
+  let _summary = ""
+  let location = ""
+  let description = "" 
+  let _start = {
+    dateTime: "",
+    timeZone: "",
+  }
+  let _end = {
+    dateTime: "",
+    timeZone: "",
+  }
+  
+  // This follows the Google Calendar API format for events.
+  let event = {
+    summary: "Google I/O 2015",
+    location: "800 Howard St., San Francisco, CA 94103",
+    description: "A chance to hear more about Google's developer products.",
+    start: {
+      dateTime: "2015-05-28T09:00:00-07:00",
+      timeZone: "America/Los_Angeles",
+    },
+    end: {
+      dateTime: "2015-05-28T17:00:00-07:00",
+      timeZone: "America/Los_Angeles",
+    },
+    reminders: {
+      useDefault: false,
+    },
+  };
+
+  return event;
+};
